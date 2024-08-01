ccc05a6686e8a4d700f5a91c420900973050f4c3
Luis <luishuapaya-trabajo@MacBook-Pro-de-Luis.local>
2024-07-31 20:43:32 -0500

    se convierte a subgrafo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
 
 const { readFileSync } = require('fs');
 const gql = require('graphql-tag');
